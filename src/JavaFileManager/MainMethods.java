aa3934a82c81f399220252c47970237fbfd9d5ed
JavaTeamAvalon <JavaTeamAvalon@mail.ru>
2017-11-27 10:21:02 +0300

    Interface with main methods

---
+++ src/JavaFileManager/MainMethods.java
@@ -0,0 +1,25 @@
+package JavaFileManager;
+
+public interface MainMethods {
+
+    public void buildingFileStructure ();
+    /*
+     * Построение файловой стурктуры
+     */
+
+    public void copyFilesToArray ();
+    /*
+     * Копирование файлов в список
+     */
+
+    public void searchFiles ();
+    /*
+     * Поиск файлов
+     */
+
+    public void saveSettings ();
+    /*
+     * Сохранение настроек
+     */
+
+}
