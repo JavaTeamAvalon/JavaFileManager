69bf9532779455d4135e0f26ad99459005e0cc99
JavaTeamAvalon <JavaTeamAvalon@mail.ru>
2017-11-27 10:33:37 +0300

    Interface with  logic methods

---
+++ src/JavaFileManager/LogicMethods.java
@@ -0,0 +1,35 @@
+package JavaFileManager;
+
+public interface LogicMethods {
+
+    public void addDateToTheEnd ();
+    /*
+     * Добавление даты в конец имени по заданному шаблону
+     */
+
+    public void addDateInFront ();
+    /*
+     * Добавление даты в начало имени по заданному шаблону
+     */
+
+    public void addPostfix ();
+    /*
+     * Добавление постфикса по заданному шаблону
+     */
+
+    public void addSuffix ();
+    /*
+     * Добавление суффикса по заданному шаблону
+     */
+
+    public void changeExpansion ();
+    /*
+     * Изменение расширения файла или папки
+     */
+
+    public void changeOwner ();
+    /*
+     * Изменение владельца файла или папки
+     */
+
+}
